d0bc63c281fe0863b6a037446680743ef7203024
fortune cookie <767077147@qq.com>
2022-11-13 15:14:52 +0800

    refactor: 渲染引擎和CLI独立成库

--- packages/cookie-cli/bin/cookie.js
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env node
-
-function start() {
-  return import('../dist/src/index.js')
-}
-start()
--- packages/render-engine/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './types/index'
-export function render() {
-	console.log('dsa222ds')
-}
-
-export function test() {
-	console.log('tesgagat..')
-}
